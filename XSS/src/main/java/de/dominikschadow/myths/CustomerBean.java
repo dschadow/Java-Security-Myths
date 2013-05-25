4382684e89989caaecf528e07c6e7f6698474417
Dominik Schadow <dominikschadow@googlemail.com>
2013-05-25 21:39:43 +0200

    Refactoring for demo project
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- XSS/src/main/java/de/dominikschadow/myths/CustomerBean.java
+++ XSS/src/main/java/de/dominikschadow/myths/CustomerBean.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.webappsecurity;
+package de.dominikschadow.myths;
 
 import java.io.Serializable;
 import java.util.LinkedHashMap;
